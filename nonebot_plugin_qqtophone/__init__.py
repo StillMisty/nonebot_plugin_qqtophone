a135b1abdd931f730f551de710ad6095d3bfe019
StillMisty <1772797842@qq.com>
2024-04-25 22:49:18 +0800

    Add files via upload

--- nonebot_plugin_qqtophone/__init__.py
+++ nonebot_plugin_qqtophone/__init__.py
@@ -10,39 +10,39 @@ __plugin_meta__ = PluginMetadata(
     name="QQ查询",
     description="通过QQ号查询手机号",
     type="application",
-    usage="查询qq [QQ号|@群友]",
+    usage="查询q [QQ号|@群友]",
 )
 
-qqtophone = on_command("查询qq",priority=5,block=True,aliases=set(["查询QQ","查询qq号","查询QQ号","查询q","查询Q"]))
+qqtophone = on_command("查询q", priority=5, block=True, aliases=set(["查询Q", "开"]))
 
-async def query_qq(qq:str):
+
+async def query_qq(qq: str):
     if qq.isdigit() and 5 <= len(qq) <= 11:
-        
         url = r"https://zy.xywlapi.cc/qqapi"
 
         try:
             async with httpx.AsyncClient() as httpx_client:
-                res = await httpx_client.get(url=url,params={"qq":qq}, timeout=10)
-            
+                res = await httpx_client.get(url=url, params={"qq": qq}, timeout=10)
+
             if res.status_code != 200:
                 return f"查询失败，服务器错误，状态码{res.status_code}"
 
             res = res.json()
             status = res.get("status")
             if status == 200:
-                return f'查询结果：\nQQ号：{res["qq"]}\n手机号：{res["phone"]}\n归属地：{res["phonediqu"]}'
+                return f'查询结果：\nQQ号:{res["qq"]}\n手机号:{res["phone"]}\n归属地:{res["phonediqu"]}'
             elif status == 500:
                 return "查询失败，信息不存在"
             else:
                 return "查询失败，未知错误"
         except Exception as e:
-            logger.error(f"QQ查询失败，{e}")
-            return f"查询失败，请稍后再试"
-        
-        
+            logger.error(f"QQ查询失败,{e}")
+            return "查询失败，请稍后再试"
+
     else:
         return "QQ号格式错误"
-            
+
+
 @qqtophone.handle()
 async def _(args: Message = CommandArg()):
     if not args:
@@ -54,7 +54,5 @@ async def _(args: Message = CommandArg()):
         if seg.type == "text":
             qq = seg.data["text"]
             break
-        
+
     await qqtophone.finish(await query_qq(qq), at_sender=True)
-    
-    
