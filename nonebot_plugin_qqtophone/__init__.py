6d0af5beb5e096c2414ea2ec4a0bdee883fa3fd8
StillMisty <1772797842@qq.com>
2024-10-18 01:18:33 +0800

    出货后20s自动撤回

--- nonebot_plugin_qqtophone/__init__.py
+++ nonebot_plugin_qqtophone/__init__.py
@@ -1,10 +1,11 @@
 from nonebot.plugin import PluginMetadata
 from nonebot import on_command
-from nonebot.adapters.onebot.v11 import Message
+from nonebot.adapters.onebot.v11 import Message, Bot, MessageEvent
 from nonebot.params import CommandArg
 from nonebot.log import logger
 
 import httpx
+from asyncio import sleep
 
 __plugin_meta__ = PluginMetadata(
     name="QQ查询",
@@ -14,35 +15,35 @@ __plugin_meta__ = PluginMetadata(
     homepage="https://github.com/StillMisty/nonebot_plugin_qqtophone",
 )
 
-qqtophone = on_command("查询q", priority=5, block=True, aliases=set(["查询Q", "开"]))
+qqtophone = on_command("开", priority=5, block=True)
 
 
-async def query_qq(qq: str):
+async def query_qq(qq: str) -> tuple[str, bool]:
     if not 5 <= len(qq) <= 11:
         return "QQ号格式错误"
     try:
-        url = r"https://zy.xywlapi.cc/qqapi"
+        url = r"https://api.xywlapi.cc/qqapi"
         async with httpx.AsyncClient() as httpx_client:
-            res = await httpx_client.get(url=url, params={"qq": qq},timeout=10)
+            res = await httpx_client.get(url=url, params={"qq": qq}, timeout=10)
     except TimeoutError:
-        return "查询失败，请稍后再试"
-    
+        return "查询失败，请稍后再试", False
+
     if res.status_code != 200:
-        return f"查询失败，服务器错误，状态码{res.status_code}"
-    
+        return f"查询失败，服务器错误，状态码{res.status_code}", False
+
     res = res.json()
     status = res.get("status")
     if status == 200:
-        return f'查询结果：\nQQ号:{res["qq"]}\n手机号:{res["phone"]}\n属地:{res["phonediqu"]}'
+        return f'查询结果：\nQQ号:{res["qq"]}\n手机号:{res["phone"]}\n属地:{res["phonediqu"]}', True
     elif status == 500:
-        return "查询失败，信息不存在"
+        return "查询失败，信息不存在", False
     else:
         logger.error(f"查询失败，未知错误，返回信息：{res}")
-        return "查询失败，未知错误"
+        return "查询失败，未知错误",    False
 
 
 @qqtophone.handle()
-async def _(args: Message = CommandArg()):
+async def _(bot: Bot, event: MessageEvent, args: Message = CommandArg()):
     if not args:
         await qqtophone.finish("请输入QQ号或@群友", at_sender=True)
     for seg in args:
@@ -52,6 +53,12 @@ async def _(args: Message = CommandArg()):
         if seg.type == "text":
             qq = seg.data["text"]
             break
-        
+    
     if qq.isdigit():
-        await qqtophone.finish(await query_qq(qq), at_sender=True)
+        msg, success = await query_qq(qq)
+        msg_id = (await bot.send(event, msg, at_sender=True))['message_id']
+        if success:
+            await sleep(20)
+            await bot.delete_msg(message_id=msg_id)
+        
+
